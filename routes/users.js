f21ebe9666f22f3cafa33fe1882d5afc958a996f
Dylan Harrison <dillysnaps@gmail.com>
2017-12-27 19:52:18 -0600

    user-model-and-register

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/user');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
 
 // register
-router.get('/register', (req, res, next) => {
-    res.send('register');
+router.post('/register', (req, res, next) => {
+
+    let newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    User.addUser(newUser, (err, user) => {
+        if (err) {
+            res.json({ success: false, msg: 'Failed to register user' });
+        } else {
+            res.json({ success: true, msg: 'User registered' });
+        }
+    });
 });
 
 // authenticate
-router.get('/authenticate', (req, res, next) => {
+router.post('/authenticate', (req, res, next) => {
     res.send('authenticate');
 });
 
